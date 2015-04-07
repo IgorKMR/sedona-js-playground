330342de80cdd01db48e771241dc8f686ff97ab9
Anna Vasilyeva <vasileva.ann@gmail.com>
2015-04-08 00:10:44 +0300

    Разбор формы Седоны

--- feedback/js/form.js
+++ feedback/js/form.js
@@ -1,74 +1,88 @@
 (function() {
 
-  var minus = document.querySelectorAll('.minus');
-  var plus = document.querySelectorAll('.plus');
-  var input = document.querySelectorAll('.counter-input');
-  var i = 0;
+  var adultsCounter = document.querySelector('.adults');
+  var childrenCounter = document.querySelector('.children');
+  var daysCounter = document.querySelector('.days');
 
-  var adults = document.querySelector('.adults');
-  var children = document.querySelector('.children');
-  var adultsInput = document.querySelector('.people-count');
-  var childrenInput = document.querySelector('.children-count');
-  var adultTemplate = document.querySelector("#adult-template").innerHTML;
-  var childTemplate = document.querySelector("#child-template").innerHTML;
-  var travelers = document.querySelector('.travelers');
+  function removeLastTraveler(container) {
+    container.removeChild(container.lastChild);
+  }
 
-  var parentInput, number, div, html;
+  function addTraveler(container, template, number) {
+    var nameBlock = document.createElement("div");
+    nameBlock.innerHTML = Mustache.render(template, { "number": number });
+    container.appendChild(nameBlock);
+  }
 
-  for (i = 0; i < minus.length; i++) {
-    minus[i].addEventListener('click', function(event) {
-      parentInput = this.parentNode.querySelector('.counter-input');
-      event.preventDefault();
+  function onKeyup() {
+    this.value = this.value.replace(/\D/g,'');
+  }
 
-      if ( this === document.activeElement ) {
-        if (parentInput.value > 1 || parentInput.name === "children-count" && parentInput.value > 0) {
-          parentInput.value--;
+  function listenCountInputChange(counterBlock) {
+    var input = counterBlock.querySelector('input');
+    var plus = counterBlock.querySelector('.plus');
+    var minus = counterBlock.querySelector('.minus');
+    var templateName = counterBlock.getAttribute('data-template');
+    var template, containerName, container, templateContent;
 
-          if ( adults === document.activeElement.parentNode) {
-            div = document.querySelector(".adult-name").parentNode.parentNode.lastChild;
-            travelers.removeChild(div);
-          }
-          else if ( children === document.activeElement.parentNode) {
-            div = document.querySelector(".child-name").parentNode.parentNode.lastChild;
-            travelers.removeChild(div);
+    input.addEventListener('keyup', onKeyup, false);
+
+    if (templateName === null) {
+      minus.addEventListener('click', function(event) {
+        event.preventDefault();
+        if (input.value > 0) {
+          input.value--;
+        }
+      }, false);
+
+      plus.addEventListener('click', function(event) {
+        event.preventDefault();
+        input.value++;
+      }, false);
+    }
+
+    else {
+      template = document.querySelector('.' + templateName);
+      containerName = counterBlock.getAttribute('data-container');
+      container = document.querySelector('.' + containerName);
+      templateContent = template.innerHTML;
+      minus.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        if ( this === document.activeElement ) {
+          if (input.value > 1 || input.name === "children-count" && input.value > 0) {
+            input.value--;
+            removeLastTraveler(container);
           }
         }
-      }
-    }, false);
+      }, false);
+
+      plus.addEventListener('click', function(event) {
+        event.preventDefault();
+        addTraveler(container, templateContent, ++input.value);
+      }, false);
+
+      input.addEventListener('blur', function(event) {
+        var currentTravelersCount = container.childNodes.length;
+        var newTravelersCount = this.value;
+        var i;
+
+        if (newTravelersCount > currentTravelersCount) {
+          for (i = currentTravelersCount + 1; i <= newTravelersCount; i++) {
+            addTraveler(container, templateContent, i);
+          }
+        }
+        else if (newTravelersCount < currentTravelersCount && newTravelersCount >= input.min) {
+          for (i = newTravelersCount; i < currentTravelersCount; i++) {
+            removeLastTraveler(container);
+          }
+        }
+      }, false);
+    }
   }
 
-  for (i = 0; i < plus.length; i++) {
-    plus[i].addEventListener('click', function(event) {
-      parentInput = this.parentNode.querySelector('.counter-input');
-      event.preventDefault();
-      parentInput.value++;
-
-      if ( adults === document.activeElement.parentNode) {
-        number = adultsInput.value;
-        div = document.createElement("div");
-        html = Mustache.render(adultTemplate, {
-          "number": number
-        });
-        div.innerHTML = html;
-        travelers.appendChild(div);
-      }
-      else if ( children === document.activeElement.parentNode) {
-        number = childrenInput.value;
-        div = document.createElement("div");
-        html = Mustache.render(childTemplate, {
-          "number": number
-        });
-        div.innerHTML = html;
-        travelers.appendChild(div);
-      }
-
-    }, false);
-  }
-
-  for (i = 0; i < input.length; i++) {
-    input[i].addEventListener('keyup', function() {
-      this.value = this.value.replace(/\D/g,'');
-    }, false);
-  }
+  listenCountInputChange(adultsCounter);
+  listenCountInputChange(childrenCounter);
+  listenCountInputChange(daysCounter);
 
 })();
